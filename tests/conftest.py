c214ba0795aee3a943c1ccb8c32b47d95270485f
Cody Scott <cody.j.b.scott@gmail.com>
2026-01-21 07:25:40 -0500

    Removed a little more

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,29 +8,19 @@ import pytest
 from utils import copy_file
 
 
-def create_directory(name: str) -> Path:
-    directory = Path(name)
-    try:
-        directory.mkdir()
-    except FileExistsError:
-        msg = f'{name} already exists and was not created.'
-    return directory
-
-
 def copy_site_file(directory: Path, filename: str) -> None:
-    if directory.name == '':
-        anchor = 'htmd.example_site'
-    else:
-        anchor = f'htmd.example_site.{directory}'
+    anchor = f'htmd.example_site.{directory}'
     source_path = files(anchor) / filename
     destination_path = directory / filename
 
     with as_file(source_path) as file:
         copy_file(file, destination_path)
 
+
 def initialize_site() -> None:
     """Create example files to get started."""
-    dir_posts = create_directory('posts/')
+    dir_posts = Path('posts/')
+    dir_posts.mkdir()
     copy_site_file(dir_posts, 'example.md')
 
 
